8d7a892d4ebb0424c5ad3450b6c6f41248a94071
jellyfishgh <1129088994@qq.com>
2016-07-10 22:27:21 +0800

    mac:delete multicast udp client and server

--- chapters/udp/multicastSendClient.js
+++
@@ -1,7 +0,0 @@
-const dgram = require('dgram');
-
-const client = dgram.createSocket('udp4');
-const message = new Buffer('this is a message.');
-client.setMulticastTTL(10);
-client.send(message, 0, message.length, 4000, '230.1.2.3');
-client.close();
